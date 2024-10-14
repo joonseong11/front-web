697f974b391244363fee7485122bbd97226c94e3
Devicii <62472550+asroq1@users.noreply.github.com>
2024-10-14 23:12:04 +0900

    FEAT : 초기 렌더링 화면 탭 구성

---
+++ src/components/EventsContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EventsContent = () => {
+  return <div>이벤트 내용</div>
+}
+
+export default EventsContent
---
+++ src/components/NewsContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewsContent = () => {
+  return <div>뉴스 내용</div>
+}
+
+export default NewsContent
