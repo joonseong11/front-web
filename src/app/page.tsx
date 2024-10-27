a3b3f23da3e8ef9e7647c04011e598e872999c99
Devicii <62472550+asroq1@users.noreply.github.com>
2024-10-27 14:44:17 +0900

    STYLES: 파비콘 및 대표 로고 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,58 @@ import NewsListGrid from '@/components/NewsListGrid'
 import { useState } from 'react'
 import Image from 'next/image'
 
+const images = {
+  mobile: {
+    src: '/mobileLogo.jpg',
+    width: 500,
+    height: 500,
+  },
+  tablet: {
+    src: '/tabletLogo.jpg',
+    width: 768,
+    height: 768,
+  },
+  desktop: {
+    src: '/webLogo.jpg',
+    width: 1000,
+    height: 1000,
+  },
+} as const
+
 export default function Home() {
   const [activeTab, setActiveTab] = useState('환경 뉴스')
 
   return (
     <div className="min-h-screen bg-gray-100">
       {/* 네비게이션 바 */}
-      <section className="w-dvw max-w-[1440px] margin-auto">
+      <section className="w-dvw max-w-[1440px] margin-auto hidden laptop:block">
         <Image
-          src="/logo.png"
+          {...images.desktop}
           alt="로고 이미지"
-          width="1000"
-          height="1000"
-          style={{ width: '100%', height: '100%' }}
+          priority
+          quality={100}
+          sizes="(min-width: 1200px) 100vw"
+          className="w-full h-auto"
+        />
+      </section>
+      <section className="hidden tablet:block laptop:hidden">
+        <Image
+          {...images.tablet}
+          alt="로고 이미지"
+          priority
+          quality={85}
+          sizes="(min-width: 600px) and (max-width: 1199px) 100vw"
+          className="w-full h-auto"
+        />
+      </section>
+      <section className="block tablet:hidden">
+        <Image
+          {...images.mobile}
+          alt="로고 이미지"
+          priority
+          quality={100}
+          sizes="(max-width: 599px) 100vw"
+          className="w-full h-auto"
         />
       </section>
       {/* 메인 콘텐츠 */}
@@ -29,7 +68,7 @@ export default function Home() {
             <button
               className={`text-gray-600 py-4 px-6 block hover:text-textLight focus:outline-none ${
                 activeTab === '환경 뉴스'
-                  ? 'text-blue-500 border-b-2 font-medium border-green'
+                  ? 'text-blue-500 border-b-2 font-medium border-text'
                   : ''
               }`}
               onClick={() => setActiveTab('환경 뉴스')}
@@ -39,7 +78,7 @@ export default function Home() {
             <button
               className={`text-gray-600 py-4 px-6 block hover:text-textLight focus:outline-none ${
                 activeTab === '지자체 행사'
-                  ? 'text-blue-500 border-b-2 font-medium border-green'
+                  ? 'text-blue-500 border-b-2 font-medium border-text'
                   : ''
               }`}
               onClick={() => setActiveTab('지자체 행사')}
