27f16cee3ca46642f28c88ddbb11069206024793
Devicii <62472550+asroq1@users.noreply.github.com>
2024-10-29 20:27:27 +0900

    CHORE: CORS 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:9000/api/:path*',
+        },
+      ]
+    }
+    return []
+  },
   images: {
     domains: [
       'picsum.photos',
